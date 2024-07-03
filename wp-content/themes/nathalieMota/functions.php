48c62eb0b74a6ed547cd75c062d28650db2b169c
GitPixel <gitpixel54@gmail.com>
2024-07-03 09:53:25 +0200

    first commit

---
+++ wp-content/themes/nathalieMota/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// Add Support Images
+add_theme_support( 'post-thumbnails' );
+
+// Automatically Add the Site Title in the Site Header
+add_theme_support( 'title-tag');
---
+++ wp-content/themes/nathalieMota/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Nathalie Mota</title>
+    </head>
+    <body class="home blog logged-in admin-bar no-customize-support">
+        
+    </body>
+</html>
+
+<?php get_header(); ?>
+
+<h1>Coucou !</h1>
+
+<?php get_footer(); ?>
